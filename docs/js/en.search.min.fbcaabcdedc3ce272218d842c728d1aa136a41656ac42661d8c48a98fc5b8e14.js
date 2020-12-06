d900bf4cd55f7a5633b7fa48ebcb65d86deb61e5
keiSunagawa <keisunagawa.git@icloud.com>
2020-12-06 17:46:04 +0900

    rename public dir

--- docs/js/en.search.min.fbcaabcdedc3ce272218d842c728d1aa136a41656ac42661d8c48a98fc5b8e14.js
+++
@@ -1,7 +0,0 @@
-'use strict';(function(){const input=document.querySelector('#gdoc-search-input');const results=document.querySelector('#gdoc-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/js/flexsearch-ad47a5e1ee.min.js');loadScript('/js/en.search-data.min.2a200dcbc9225470d43753a4cd8b8e4ee89f73a721bb133c3d0d556e1f4c47e6.js',function(){input.required=false;search();});}
-function search(){while(results.firstChild){results.removeChild(results.firstChild);}
-if(!input.value){console.log("empty")
-results.classList.remove("has-hits");return;}
-const searchHits=window.geekdocSearchIndex.search(input.value,10);console.log(searchHits.length);if(searchHits.length>0){results.classList.add("has-hits");}else{results.classList.remove("has-hits");}
-searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);results.classList.add("DUMMY");});}
-function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
